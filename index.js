f3702db04274c71502da9111546d98a5aaaaf716
Xiahui Zhang <xhz0729@gmail.com>
2024-07-31 23:57:59 -0500

    validate email and phone number

---
+++ index.js
@@ -0,0 +1,19 @@
+// validate email and phone number
+document.getElementById('info-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    var phoneNum = document.getElementById('phone-number').value;
+  
+    if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      alert('Invalid email address');
+      return false;
+    }
+
+    if (!phoneNum.match(/^[\+]?[0-9]{0,3}\W?+[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+        alert('Invalid phone number');
+        return false;
+
+    }
+    document.getElementById('info-form').reset();
+    console.log('Form is valid');
+  });
