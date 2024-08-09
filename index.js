648b60f809e74e8fffe3235be8bbb3a9167fd9e2
Xiahui Zhang <xhz0729@gmail.com>
2024-08-08 20:10:17 -0500

    formatting my js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 // validate email and phone number
-document.getElementById('info-form').addEventListener('submit', function(event) {
+document
+  .getElementById("info-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    var email = document.getElementById('email').value;
-    var phoneNum = document.getElementById('phone-number').value;
-  
+    const email = document.getElementById("email").value;
+    const phoneNum = document.getElementById("phone-number").value;
+
     if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-      alert('Invalid email address');
+      alert("Invalid email address");
       return false;
     }
-
-    if (!phoneNum.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
-        alert('Invalid phone number');
-        return false;
-
+    
+    if (
+      !phoneNum.match(
+        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+      )
+    ) {
+      alert("Invalid phone number");
+      return false;
     }
-    document.getElementById('info-form').reset();
-    console.log('Form is valid');
+    document.getElementById("info-form").reset();
   });
 
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
+const video = document.getElementById("myVideo");
+const btn = document.getElementById("myBtn");
 
 function myFunction() {
   if (video.paused) {
